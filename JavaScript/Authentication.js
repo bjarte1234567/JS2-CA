3bbe4560d2be48427ded967616908fe486fc1dd2
bjarte oliversen løken <114422934+bjarte1234567@users.noreply.github.com>
2023-09-20 11:44:26 +0200

    new files added, an bootstrap added to html

---
+++ JavaScript/Authentication.js
@@ -0,0 +1,7 @@
+{
+    "name": "my_username", // Required
+    "email": "first.last@stud.noroff.no", // Required
+    "password": "UzI1NiIsInR5cCI", // Required
+    "avatar": "https://img.service.com/avatar.jpg", // Optional
+    "banner": "https://img.service.com/banner.jpg"      // Optional
+  }
