8db6971b8e49c2f8e94d6428e6e3e7667a4f9c48
bjarte oliversen løken <114422934+bjarte1234567@users.noreply.github.com>
2023-09-26 15:50:25 +0200

    added gitignore to node_modules

--- JavaScript/Authentication.js
+++
@@ -1,7 +0,0 @@
-{
-    "name": "my_username", // Required
-    "email": "first.last@stud.noroff.no", // Required
-    "password": "UzI1NiIsInR5cCI", // Required
-    "avatar": "https://img.service.com/avatar.jpg", // Optional
-    "banner": "https://img.service.com/banner.jpg"      // Optional
-  }
---
+++ JavaScript/loginAndRegister.js
@@ -0,0 +1,87 @@
+const API_BASE_URL = "https://api.noroff.dev/api/v1";
+
+/**
+ *
+ * @param {string} url
+ * @param {any} userData
+ * ```js
+ * registerUser(registerUrl, userToRegister);
+ * ```
+ */
+async function registerUser(url, userData) {
+  try {
+    const postData = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    };
+    const response = await fetch(url, postData);
+    console.log(response);
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error("Error during registration:", error);
+  }
+}
+
+//USER INFO
+const userToRegister = {
+  name: "Sven_manneng",
+  email: "bjarteeoliversenloken@noroff.no",
+  password: "oWiiHoo17h543gggg",
+  avatar: "",
+  banner: "",
+};
+
+const registerUrl = `${API_BASE_URL}/social/auth/register`;
+
+registerUser(registerUrl, userToRegister);
+
+//LOGIN
+/**
+ * Logs in a user by sending a POST request to the provided URL with user data.
+ *
+ * @param {string} url
+ * @param {Object} userData
+ */
+
+
+//Get token
+
+/**
+ * Retrieves an access token by sending a GET request to the provided URL with the token in the headers.
+ *
+ * @param {string} url
+ * @returns {Promise<Object>}
+ * @throws {Error}
+ */
+async function getAccessToken(url) {
+  try {
+    const token = localStorage.getItem("accessToken");
+    const fetchOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await fetch(url, fetchOptions);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Could not get accesstoken:", error);
+    throw error;
+  }
+}
+
+const postsUrl = `${API_BASE_URL}/social/posts`;
+
+getAccessToken(postsUrl)
+  .then((data) => {
+    console.log("Data catch:", data);
+  })
+  .catch((error) => {
+    console.error("Something went wrong:", error);
+  });
